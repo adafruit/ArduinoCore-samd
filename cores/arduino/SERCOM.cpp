adafruit/ArduinoCore-samd

commit ccfc7db988fe2f61dd3f8734794fa9f69730f58d

Add files via upload

cores/arduino/SERCOM.cpp
@@ -559,6 +559,12 @@ bool SERCOM::startTransmissionWIRE(uint8_t address, SercomWireReadWriteFlag flag
     while( !sercom->I2CM.INTFLAG.bit.MB )
     {
       // Wait transmission complete
+
+		// If certain errors occur, the MB bit may never be set (RFTM: SAMD21 sec:28.10.6; SAMD51 sec:36.10.7).
+		// There are additional errors that can occur (including BUSERR) that are rolled up in INTFLAG.ERROR
+		if (sercom->I2CM.INTFLAG.bit.ERROR) {
+		  return false;
+		}
     }
   }
   else  // Read mode
@@ -566,8 +572,9 @@ bool SERCOM::startTransmissionWIRE(uint8_t address, SercomWireReadWriteFlag flag
     while( !sercom->I2CM.INTFLAG.bit.SB )
     {
         // If the slave NACKS the address, the MB bit will be set.
+		// A variety of errors in the STATUS register can set the ERROR bit in the INTFLAG register
         // In that case, send a stop condition and return false.
-        if (sercom->I2CM.INTFLAG.bit.MB) {
+        if (sercom->I2CM.INTFLAG.bit.ERROR || sercom->I2CM.INTFLAG.bit.MB) {
             sercom->I2CM.CTRLB.bit.CMD = 3; // Stop condition
             return false;
         }
@@ -600,7 +607,8 @@ bool SERCOM::sendDataMasterWIRE(uint8_t data)
 
     // If a bus error occurs, the MB bit may never be set.
     // Check the bus error bit and bail if it's set.
-    if (sercom->I2CM.STATUS.bit.BUSERR) {
+    // There are additional errors that can occur (including BUSERR) that are rolled up in INTFLAG.ERROR
+    if (sercom->I2CM.INTFLAG.bit.ERROR) {
       return false;
     }
   }
@@ -704,6 +712,15 @@ uint8_t SERCOM::readDataWIRE( void )
     while( sercom->I2CM.INTFLAG.bit.SB == 0 )
     {
       // Waiting complete receive
+	  // A variety of errors in the STATUS register can set the ERROR bit in the INTFLAG register
+      // In that case, send a stop condition and return false.
+	  // readDataWIRE should really be able to indicate an error (which would never be used
+	  // because the readDataWIRE caller should have checked availableWIRE() first and timed it 
+	  // out if the data never showed up
+        if (sercom->I2CM.INTFLAG.bit.ERROR || sercom->I2CM.INTFLAG.bit.MB) {
+            sercom->I2CM.CTRLB.bit.CMD = 3; // Stop condition
+            return 0xFF;
+        }
     }
 
     return sercom->I2CM.DATA.bit.DATA ;

commit 289a27209373d6e94eb5c8430837636c8236dfef

I2C: clear ERROR flag before checking it
Commit ccfc7db988fe2f61dd3f8734794fa9f69730f58d introduced checks for
INTFLAG::ERROR.

From the datasheet:
  Bit 7 – ERROR Error
  This flag is cleared by writing '1' to it.
  This bit is set when any error is detected. Errors that will set this
  flag have corresponding status bits in the STATUS register. These status
  bits are LENERR, SEXTTOUT, MEXTTOUT, LOWTOUT, ARBLOST, and BUSERR.
  Writing '0' to this bit has no effect.
  Writing '1' to this bit will clear the flag.

Also restore the change before commit 34d15d0f9986c5311031d2737a68eaca198836ff,
as the missing clear of the flag appears to cause the problem.

cores/arduino/SERCOM.cpp
@@ -562,6 +562,7 @@ bool SERCOM::startTransmissionWIRE(uint8_t address, SercomWireReadWriteFlag flag
   }
 
   // Send start and address
+  sercom->I2CM.INTFLAG.bit.ERROR = 1;
   sercom->I2CM.ADDR.reg = SERCOM_I2CM_ADDR_ADDR(address) | 
                           ((sercom->I2CM.CTRLA.bit.SPEED == 0x2) ? SERCOM_I2CM_ADDR_HS : 0);
 
@@ -611,13 +612,16 @@ bool SERCOM::startTransmissionWIRE(uint8_t address, SercomWireReadWriteFlag flag
 bool SERCOM::sendDataMasterWIRE(uint8_t data)
 {
   //Send data
+  sercom->I2CM.INTFLAG.bit.ERROR = 1;
   sercom->I2CM.DATA.bit.DATA = data;
 
   //Wait transmission successful
   while(!sercom->I2CM.INTFLAG.bit.MB) {
     // If a data transfer error occurs, the MB bit may never be set.
     // Check the error bit and bail if it's set.
-    if (sercom->I2CM.STATUS.bit.BUSERR) {
+    // The data transfer errors that can occur (including BUSERR) are all
+    // rolled up into INTFLAG.bit.ERROR from STATUS.reg
+    if (sercom->I2CM.INTFLAG.bit.ERROR) {
       return false;
     }
   }